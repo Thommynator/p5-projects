2d96596480dca46fa2e010e94f475b4c7100e26b
Thommynator <Thommynator@users.noreply.github.com>
2017-05-06 12:34:01 +0200

    Converted Processing prject "PendulumWave" to p5.js Signed-off-by: Thommynator <Thommynator@users.noreply.github.com>

---
+++ PendulumWave/PendulumWave.js
@@ -0,0 +1,37 @@
+/**
+ * Creates a list of several pendulums, which form a wave and synch over time.
+ * The "cycleTime" parameter defines, after how many periods, the pendulums will
+ * be synched again. 
+ */
+
+var origin = new p5.Vector(300, 15);
+var pends = [];
+var t = 0;
+var cycleTime = 16.0; // pendulums are lined up, after this amount of periods
+
+function setup() {
+  createCanvas(600, 600);
+  ellipseMode(CENTER);
+
+	// amount of different pendulums
+  var nPendulums = 15;
+  for (i=0; i<nPendulums; i++) {
+    pends.push(new Pendulum(computeLength(cycleTime+i, height-(height*0.1)), map(i,0,nPendulums,18,4)));
+  }
+}
+
+function draw() {
+  background(30);
+  fill(100);
+  ellipse(origin.x, origin.y, 4, 4);
+  for (i=0; i<pends.length; i++) {
+    pends[i].updateAngle(t);
+    pends[i].show();
+  }
+
+  t += 0.5;
+}
+
+function computeLength(T, maxL) {
+  return maxL*pow(cycleTime/T, 2);
+}
