318431744ca275ab205f336d4cb2d0a1358c4cc0
Thommynator <T.SchindlerMail@gmail.com>
2018-01-16 22:12:56 +0100

    Removed semicolons

--- GameOfLife/GameOfLife.js
+++ GameOfLife/GameOfLife.js
@@ -1,63 +1,63 @@
-var nInitialCellsProb = 0.4;
-var cellcreateCanvas = 10;
-var rows;
-var cols;
-var cellsAlive = [];
-var newGeneration = [];
-var it = 0;
+var nInitialCellsProb = 0.4
+var cellcreateCanvas = 10
+var rows
+var cols
+var cellsAlive = []
+var newGeneration = []
+var it = 0
 
 function setup() {
-  var myCanvas = createCanvas(500, 500);
-  myCanvas.parent("GameOfLife");
-  background(255);
-  rows = height / cellcreateCanvas;
-  cols = width / cellcreateCanvas;
-  initializeGrid();
+  var myCanvas = createCanvas(500, 500)
+  myCanvas.parent("GameOfLife")
+  background(255)
+  rows = height / cellcreateCanvas
+  cols = width / cellcreateCanvas
+  initializeGrid()
 }
 
 function draw() {
   // rules
   for (var y = 0; y < rows; y++) {
     for (var x = 0; x < cols; x++) {
-      var n = countLivingNeighbors(y, x);
-      var idx = convertRowCol2Idx(y, x);
+      var n = countLivingNeighbors(y, x)
+      var idx = convertRowCol2Idx(y, x)
 
       // 1. a dead cell, which has exactly 3 living neighbors will be alive in the next generation
       if (!cellsAlive[idx] && n == 3) {
-        newGeneration[idx] = true;
+        newGeneration[idx] = true
       }
 
       // 2. a living cell, which has less than 2 living neighbors will die in the next generation
       if (cellsAlive[idx] && n < 2) {
-        newGeneration[idx] = false;
+        newGeneration[idx] = false
       }
 
       // 3. a living cell, which has 2 or 3 living neighbors will stay alive in the next generation
       if (cellsAlive[idx] && (n == 2 || n == 3)) {
-        newGeneration[idx] = true;
+        newGeneration[idx] = true
       }
 
       // 4. a living cell, which has more than 3 living cells will die in the next generation
       if (cellsAlive[idx] && n > 3) {
-        newGeneration[idx] = false;
+        newGeneration[idx] = false
       }
     }
   }
 
-  cellsAlive = newGeneration.slice(0);
+  cellsAlive = newGeneration.slice(0)
   for (var y = 0; y < rows; y++) {
     for (var x = 0; x < cols; x++) {
       if (cellsAlive[convertRowCol2Idx(y, x)]) {
-        fill(0, 255, 0);
+        fill(0, 255, 0)
       } else {
-        fill(50, 50, 50);
+        fill(50, 50, 50)
       }
-      rect(x * cellcreateCanvas, y * cellcreateCanvas, cellcreateCanvas, cellcreateCanvas);
+      rect(x * cellcreateCanvas, y * cellcreateCanvas, cellcreateCanvas, cellcreateCanvas)
     }
   }
 
   if (mouseIsPressed) {
-    initializeGrid();
+    initializeGrid()
   }
 }
 
@@ -65,31 +65,31 @@ function draw() {
  * Choose a cell, defined by row and col and count its alive neighbors.
  */
 function countLivingNeighbors(row, col) {
-  var c = 0;
+  var c = 0
 
   for (var i = -1; i < 2; i++) {
     for (var j = -1; j < 2; j++) {
       // if checkcell is equal to center cell
       if (i == 0 && j == 0) {
-        continue;
+        continue
       }
       // if check cell is outside of grid
       if (col + j < 0 || col + j >= cols || row + i < 0 || row + i >= rows) {
-        continue;
+        continue
       }
       if (cellsAlive[convertRowCol2Idx(row + i, col + j)]) {
-        c++;
+        c++
       }
     }
   }
-  return c;
+  return c
 }
 
 /**
  * Convert a row & col index varo an 1-dimensional array index.
  */
 function convertRowCol2Idx(row, col) {
-  return col + row * cols;
+  return col + row * cols
 }
 
 
@@ -97,15 +97,15 @@ function convertRowCol2Idx(row, col) {
  * Initialize a new grid
  */
 function initializeGrid() {
-  cellsAlive = [];
+  cellsAlive = []
   for (var y = 0; y < rows; y++) {
     for (var x = 0; x < cols; x++) {
       if (random(1) > nInitialCellsProb) {
-        cellsAlive.push(true);
+        cellsAlive.push(true)
       } else {
-        cellsAlive.push(false);
+        cellsAlive.push(false)
       }
     }
   }
-  newGeneration = cellsAlive.slice(0);
+  newGeneration = cellsAlive.slice(0)
 }
--- PendulumWave/Pendulum.js
+++ PendulumWave/Pendulum.js
@@ -1,22 +1,22 @@
 function Pendulum (pendulumLength, radius) {
-    this.l = pendulumLength;
-    this.phi = 0;
-    this.r = radius;
-    this.increaseAngle = true;
-    this.range = 0.4;
-    this.clr = color(map(this.l, 0, 500, 80,240));
+    this.l = pendulumLength
+    this.phi = 0
+    this.r = radius
+    this.increaseAngle = true
+    this.range = 0.4
+    this.clr = color(map(this.l, 0, 500, 80,240))
 
 
 	this.show = function() {
-		fill(this.clr);
-		stroke(150, 128);
-		line(origin.x, origin.y, (this.l-this.r)*sin(this.phi) + width/2, (this.l-this.r)*cos(this.phi));
-		stroke(0);
-		ellipse(this.l*sin(this.phi) + width/2, this.l*cos(this.phi), this.r*2, this.r*2);
+		fill(this.clr)
+		stroke(150, 128)
+		line(origin.x, origin.y, (this.l-this.r)*sin(this.phi) + width/2, (this.l-this.r)*cos(this.phi))
+		stroke(0)
+		ellipse(this.l*sin(this.phi) + width/2, this.l*cos(this.phi), this.r*2, this.r*2)
 	}
 
 		this.updateAngle = function(t) {
-		var omega = sqrt(9.81/this.l);
-		this.phi = this.range * sin(omega*t);
+		var omega = sqrt(9.81/this.l)
+		this.phi = this.range * sin(omega*t)
 	}
 }
